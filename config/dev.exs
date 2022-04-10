0d3265d2fbf9afcfa410d56e0a9d4131ec1f874a
F3900699 Hamilton Geraldo Fantin <hamiltonfantin@bb.com.br>
2022-04-10 11:51:36 -0300

    config dev db

--- config/dev.exs
+++ config/dev.exs
@@ -2,10 +2,10 @@ import Config
 
 # Configure your database
 config :treinos, Treinos.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "admin",
+  password: "admin",
   hostname: "localhost",
-  database: "treinos_dev",
+  database: "treinos",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
--- config/test.exs
+++ config/test.exs
@@ -6,10 +6,10 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :treinos, Treinos.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "admin",
+  password: "admin",
   hostname: "localhost",
-  database: "treinos_test#{System.get_env("MIX_TEST_PARTITION")}",
+  database: "treinos#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10
 
